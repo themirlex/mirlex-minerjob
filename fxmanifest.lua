6a88522548dc245b9a52888cb0f617dd1b0310c6
mirlex <107887309+themirlex@users.noreply.github.com>
2022-08-07 15:29:19 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'mirlex jobs'
+version '1.0.0'
+
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@PolyZone/CircleZone.lua", "client/*.lua", "config.lua"}
+
+server_scripts {
+    'server/*.lua'
+}
