4c8d4b072488ac3faaee6a0bc6abc40241423d5a
mirlex <107887309+themirlex@users.noreply.github.com>
2022-08-07 15:31:51 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,8 @@ game 'gta5'
 description 'mirlex jobs'
 version '1.0.0'
 
-client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@PolyZone/CircleZone.lua", "client/*.lua", "config.lua"}
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@PolyZone/CircleZone.lua", "client.lua"}
 
 server_scripts {
-    'server/*.lua'
+    'server.lua'
 }
